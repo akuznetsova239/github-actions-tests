e76cc71217af9ad8de7cca9431676b7a47d57a0d
akuznetsova239 <anastasia.kuznetsova@trivago.com>
2024-01-30 13:12:23 +0100

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+       Main my_class= main();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void add() {
+    }
+}
